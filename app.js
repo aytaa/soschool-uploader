605136a5904fddeb567baa0c6d8a671325eb5aea
aytaa <aytac.unal@mertech.com.tr>
2024-09-01 15:30:09 +0300

    detail log

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const path = require('path');
 const {v4: uuidv4} = require('uuid');
 const {PDFDocument} = require('pdf-lib');
 const moment = require('moment');
-const morgan = require('morgan'); // Morgan'ı dahil edin
+const morgan = require('morgan');
 const app = express();
 const PORT = 5000;
 
 // Morgan'ı kullan
-app.use(morgan('combined')); // 'combined' formatını kullanarak gelen istekleri loglar
+app.use(morgan('combined'));
 
 // 'uploads' klasörünün varlığını kontrol et ve yoksa oluştur
 const uploadDir = path.join(__dirname, 'uploads');
@@ -38,10 +38,14 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
-        if (path.extname(file.originalname).toLowerCase() === '.pdf') {
+        const fileType = path.extname(file.originalname).toLowerCase();
+        console.log(`File type: ${fileType}, Original name: ${file.originalname}, Mimetype: ${file.mimetype}`);
+
+        if (fileType === '.pdf') {
             cb(null, true);
         } else {
-            cb(null, false);
+            console.log('File rejected: Only .pdf files are allowed.');
+            cb(new Error('Only .pdf files are allowed!'), false);
         }
     }
 }).single('file');
@@ -53,14 +57,17 @@ app.use(express.urlencoded({extended: true}));
 app.post('/upload', (req, res) => {
     upload(req, res, async (err) => {
         if (err) {
+            console.log(`Error during file upload: ${err.message}`);
             return res.status(500).send({
                 status: false,
-                message: 'Failed to upload file', error: err.message,
+                message: 'Failed to upload file',
+                error: err.message,
                 date: moment().format('YYYY-MM-DD HH:mm:ss')
             });
         }
 
         if (!req.file) {
+            console.log('No file uploaded or file type is not PDF.');
             return res.status(404).send({
                 status: false,
                 message: 'Only .pdf files are allowed!',
@@ -69,6 +76,7 @@ app.post('/upload', (req, res) => {
         }
 
         try {
+            console.log(`File uploaded successfully: ${req.file.filename}`);
             const inputPath = path.join(__dirname, 'uploads', req.file.filename);
             const outputPath = path.join(__dirname, 'uploads', `compressed_${req.file.filename}`);
 
@@ -82,6 +90,7 @@ app.post('/upload', (req, res) => {
                 date: moment().format('YYYY-MM-DD HH:mm:ss')
             });
         } catch (error) {
+            console.log(`Error during PDF compression: ${error.message}`);
             res.status(500).send({
                 message: 'Failed to compress PDF',
                 error: error.message,
